db820c02b461c89086ebe26cd806857d65e37969
emilyhu <ehu@caltech.edu>
2025-02-19 17:13:28 -0800

    added calib .inos

--- code/BLDCDriver/BLDCDriver.ino
+++ code/BLDCDriver/BLDCDriver.ino
@@ -2,28 +2,34 @@
 #include <SimpleFOC.h>
 
 // BLDC driver instance
-BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
+BLDCDriver3PWM driver = BLDCDriver3PWM(11, 10, 9, 8);
 
 void setup() {
-  
-  // pwm frequency to be used [Hz]
-  driver.pwm_frequency = 20000;
-  // power supply voltage [V]
-  driver.voltage_power_supply = 12;
-  // Max DC voltage allowed - default voltage_power_supply
-  driver.voltage_limit = 12;
 
   // driver init
-  driver.init();
+  if (driver.init()) Serial.println("success!");
+  else{
+    Serial.println("failed!");
+    return;
+  }
+  
+  // pwm frequency to be used [Hz]
+  driver.pwm_frequency = 32000;
+  // power supply voltage [V]
+  driver.voltage_power_supply = 24;
+  // Max DC voltage allowed - default voltage_power_supply
+  // driver.voltage_limit = 12;
 
   // enable driver
   driver.enable();
+
+  Serial.begin(115200);
 
   _delay(1000);
 }
 
 void loop() {
-    // setting pwm
-    // phase A: 3V, phase B: 6V, phase C: 5V
-    driver.setPwm(3,6,5);
+  // setting pwm
+  // phase A: 3V, phase B: 6V, phase C: 5V
+  driver.setPwm(3,6,5);
 }
